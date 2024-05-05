53e1202d067cca9b2d5566e1fe225dc1be5879d7
Jardiel <jardiel_santoss@outlook.com>
2024-05-05 12:06:55 -0300

    Criando modelo de prato

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Prato(models.Model):
+    OPCOES_CATEGORIAS = [
+        ("LANCHE", "Lanche"),
+        ("PIZZA", "Pizza"),
+        ("HOTDOG", "Hotdog")
+    ]
+
+    nome = models.CharField(max_length=50, null= False, blank= False)
+    descrição = models.CharField(max_length=100, null= False, blank= False)
+    fotografia = models.ImageField(upload_to="fotos_pratos", blank= False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS, default='')
