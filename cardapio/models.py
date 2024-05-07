995c30faf6338458d3cf4e3337d6a8cb9b1cfba1
Bruno Tiecher <brunotiecher@hotmail.com>
2024-05-07 15:07:16 -0300

    Adicionando o metodo excluir das imagens

--- cardapio/models.py
+++ cardapio/models.py
@@ -12,6 +12,10 @@ class Prato(models.Model):
     fotografia = models.ImageField(upload_to="media/fotos_pratos", blank= False)
     categoria = models.CharField(max_length=20, choices=OPCOES_CATEGORIAS_PRATOS, default='')
 
+    def delete(self, *args, **kwargs):
+        self.fotografia.delete(save=False)
+        super().delete(*args, **kwargs)
+        
     def __str__(self):
         return self.nome
 
@@ -25,3 +29,10 @@ class Bebida(models.Model):
     descricao = models.CharField(max_length=100, null= False, blank= False)
     fotografia = models.ImageField(upload_to="media/fotos_bebidas", blank= False)
     categoria = models.CharField(max_length=20, choices=OPCOES_CATEGORIAS_BEBIDAS, default='')
+    
+    def delete(self, *args, **kwargs):
+        self.fotografia.delete(save=False)
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return self.nome
