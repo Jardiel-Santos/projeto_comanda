95be5cb10e5d7b0b1b400c823b1271b203561e36
Jardiel <jardiel_santoss@outlook.com>
2024-05-06 23:58:13 -0300

    Criando class Bebida

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Prato(models.Model):
-    OPCOES_CATEGORIAS = [
+    OPCOES_CATEGORIAS_PRATOS = [
         ("LANCHE", "Lanche"),
         ("PIZZA", "Pizza"),
         ("HOTDOG", "Hotdog")
@@ -10,7 +10,19 @@ class Prato(models.Model):
     nome = models.CharField(max_length=50, null= False, blank= False)
     descrição = models.CharField(max_length=100, null= False, blank= False)
     fotografia = models.ImageField(upload_to="fotos_pratos", blank= False)
-    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS, default='')
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIAS_PRATOS, default='')
 
     def __str__(self):
         return self.nome
+
+class Bebida(models.Model):
+    OPCOES_CATEGORIAS_BEBIDAS = [
+        ("REFRIGERANTES", "Refrigerantes"),
+        ("ALCOOLICAS", "Alcoolicas"),
+    ]
+
+    nome = models.CharField(max_length=50, null= False, blank= False)
+    descricao = models.CharField(max_length=100, null= False, blank= False)
+    fotografia = models.ImageField(upload_to="fotos_bebidas", blank= False)
+    categoria = models.CharField(max_length=10, choices=OPCOES_CATEGORIAS_BEBIDAS,
+                                 )
