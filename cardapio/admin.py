805a6b13d4dc26498a65bcd26594faa96770ebe3
Bruno Tiecher <brunotiecher@hotmail.com>
2024-05-07 10:24:44 -0300

    Adicionando bebidas ao menu admin

--- cardapio/admin.py
+++ cardapio/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from cardapio.models import Prato
+from cardapio.models import Prato, Bebida
 
 class ListandoPratos(admin.ModelAdmin):
+    list_display = ("nome", "descrição", "fotografia", "categoria")
+
+
+class ListandoBebidas(admin.ModelAdmin):
     list_display = ("nome", "descrição", "fotografia", "categoria")
 
 
 
 
 admin.site.register(Prato, ListandoPratos)
+admin.site.register(Bebida, ListandoBebidas)
--- cardapio/models.py
+++ cardapio/models.py
@@ -22,7 +22,6 @@ class Bebida(models.Model):
     ]
 
     nome = models.CharField(max_length=50, null= False, blank= False)
-    descricao = models.CharField(max_length=100, null= False, blank= False)
+    descrição = models.CharField(max_length=100, null= False, blank= False)
     fotografia = models.ImageField(upload_to="fotos_bebidas", blank= False)
-    categoria = models.CharField(max_length=10, choices=OPCOES_CATEGORIAS_BEBIDAS,
-                                 )
+    categoria = models.CharField(max_length=13, choices=OPCOES_CATEGORIAS_BEBIDAS,)
