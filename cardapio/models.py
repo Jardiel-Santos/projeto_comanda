0bc5441ddc59ab412d1ff18e76e9f51bbb72f1a1
Jardiel <jardiel_santoss@outlook.com>
2024-05-15 21:52:03 -0300

    Arrumando o media, add valor e começando pedidos

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from decimal import Decimal
+from djmoney.models.fields import MoneyField
+from djmoney.models.validators import MaxMoneyValidator, MinMoneyValidator
 
 class Prato(models.Model):
     OPCOES_CATEGORIAS_PRATOS = [
@@ -9,12 +12,13 @@ class Prato(models.Model):
 
     nome = models.CharField(max_length=50, null= False, blank= False)
     descricao = models.CharField(max_length=100, null= False, blank= False)
-    foto = models.ImageField(upload_to="media/fotos_pratos", blank= True)
+    foto = models.ImageField(upload_to="fotos_pratos", blank= True)
     categoria = models.CharField(max_length=20, choices=OPCOES_CATEGORIAS_PRATOS, default='')
+    valor = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal(0))
 
     """Remove as fotos da pasta media"""
     def delete(self, *args, **kwargs):
-        self.fotografia.delete(save=False)
+        self.foto.delete(save=False)
         super().delete(*args, **kwargs)
         
     def __str__(self):
@@ -28,12 +32,21 @@ class Bebida(models.Model):
 
     nome = models.CharField(max_length=50, null= False, blank= False)
     descricao = models.CharField(max_length=100, null= False, blank= False)
-    foto = models.ImageField(upload_to="media/fotos_bebidas", blank= False)
+    foto = models.ImageField(upload_to="fotos_bebidas", blank= False)
     categoria = models.CharField(max_length=20, choices=OPCOES_CATEGORIAS_BEBIDAS, default='')
+   
+    # TESTE DO MONEYFIELD
+   
+    # valor = MoneyField(
+    #     max_digits=5, decimal_places=2, default=0, default_currency='BRL',
+    #     validators=[
+    #         MinMoneyValidator(Decimal(0.00)), MaxMoneyValidator(Decimal(999.99)),
+    #     ]
+    # )
     
     """Remove as fotos da pasta media"""
     def delete(self, *args, **kwargs):
-        self.fotografia.delete(save=False)
+        self.foto.delete(save=False)
         super().delete(*args, **kwargs)
 
     def __str__(self):
