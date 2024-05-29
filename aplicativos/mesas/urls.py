eb6c2fcd1473d897ea6b66418d63515d1ae029e9
Jardiel <jardiel_santoss@outlook.com>
2024-05-29 08:21:11 -0300

    Criando acesso as mesas

--- aplicativos/mesas/urls.py
+++ aplicativos/mesas/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from aplicativos.mesas.views import mesas, index
+from aplicativos.mesas.views import mesas, index, mesa_id
 
 urlpatterns = [
     path('', index),
-    path('mesas', mesas, name= 'mesas')
+    path('mesas', mesas, name= 'mesas'),
+    path('mesas/<int:mesa_id>', mesa_id, name= 'mesa_id'),
 ]
--- aplicativos/mesas/views.py
+++ aplicativos/mesas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from aplicativos.mesas.models import Mesa
 
 def index(request):
@@ -7,3 +7,7 @@ def index(request):
 def mesas(request):
     mesas = Mesa.objects.all()
     return render(request, 'mesas.html', {"mesas": mesas})
+
+def mesa_id(request, mesa_id):
+    mesa_id = get_object_or_404(Mesa, pk=mesa_id)
+    return render(request, 'mesa_id.html', {"mesas": mesa_id})
