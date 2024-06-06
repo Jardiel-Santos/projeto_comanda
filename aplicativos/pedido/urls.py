d1f886cfc0dc90751d02580b1d76b58f87c46e24
Bruno Tiecher <brunotiecher@hotmail.com>
2024-06-06 11:56:00 -0300

    Ajustando pedido de bebidas

--- aplicativos/pedido/urls.py
+++ aplicativos/pedido/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from aplicativos.pedido.views import index, pedido, imagem_prato, imagem_bebida, adicionar_prato_a_comanda, adicionar_bebida_a_comanda
 
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('mesas/<int:mesa_id>/pedido', pedido, name= 'pedido'),
     path('mesas/<int:mesa_id>/pedido/imagem/prato/<int:prato_id>', imagem_prato, name= 'imagem_prato'),
     path('mesas/<int:mesa_id>/pedido/imagem/bebida/<int:bebida_id>', imagem_bebida, name= 'imagem_bebida'),
-    path('adicionar_a_comanda/<int:mesa_id>/<int:prato_id>', adicionar_prato_a_comanda, name= 'adicionar_prato_a_comanda'),
-    path('adicionar_a_comanda/<int:mesa_id>/<int:bebida_id>', adicionar_bebida_a_comanda, name= 'adicionar_bebida_a_comanda'),
+    path('adicionar_prato_a_comanda/<int:mesa_id>/<int:prato_id>', adicionar_prato_a_comanda, name= 'adicionar_prato_a_comanda'),
+    path('adicionar_bebida_a_comanda/<int:mesa_id>/<int:bebida_id>', adicionar_bebida_a_comanda, name= 'adicionar_bebida_a_comanda'),
 ]
 
